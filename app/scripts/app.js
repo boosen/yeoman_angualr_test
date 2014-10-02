5f3dd73e5e7bfad1fd5963cbd1bcea2fda991e8d
Bushen Shi <bushen.shi@hotmail.com>
2014-10-02 15:09:33 +0800

    created an new angular test with yeoman (npm, bower, grunt)

---
+++ app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name myAngularApp
+ * @description
+ * # myAngularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('myAngularApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.sortable'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
